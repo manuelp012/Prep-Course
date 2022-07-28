43519b2a91f3fc20ee2e88e6314a31042f9cd021
Manuel <manuelangel_1203@hotmail.com>
2022-07-28 07:51:26 -0300

    first commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -84,23 +84,15 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(numero === 10 || 5){
-    return true
-  } else  { 
-    return false
-  }
-  // encontrar el codigo correcto 
+  return numero === 10 || numero === 5
+  
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (numero < 50 && numero > 20){
-    return true 
-  } else {
-    return false
-  }
+  return numero < 50 && numero > 20;
 }
 
 function esEntero(numero) {
@@ -111,6 +103,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero % 1 === 0){
+    return true
+  } else {
+    return false
+  }
 }
 
 function fizzBuzz(numero) {
